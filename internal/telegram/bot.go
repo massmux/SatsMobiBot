eeb9d5224efc2629a0831235c147cfbe4da63a16
LightningTipBot <88730856+LightningTipBot@users.noreply.github.com>
2021-12-01 07:11:49 +0100

    revert (#148)
    Co-authored-by: callebtc <93376500+callebtc@users.noreply.github.com>

--- internal/telegram/bot.go
+++ internal/telegram/bot.go
@@ -54,7 +54,7 @@ func NewBot() TipBot {
 func newTelegramBot() *tb.Bot {
 	tgb, err := tb.NewBot(tb.Settings{
 		Token:     internal.Configuration.Telegram.ApiKey,
-		Poller:    &tb.LongPoller{Timeout: 60 * time.Second, AllowedUpdates: []string{"message"}},
+		Poller:    &tb.LongPoller{Timeout: 60 * time.Second},
 		ParseMode: tb.ModeMarkdown,
 	})
 	if err != nil {
