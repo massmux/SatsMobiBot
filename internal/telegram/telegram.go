25d9b8af3bb2a56fc082ef0f7104a6d31c4f9279
gohumble <55599638+gohumble@users.noreply.github.com>
2021-12-06 20:08:33 +0100

    Check admin rights  (#152)
    * add rightsToDoAction
    
    * comments
    
    * comments
    
    * typo
    
    * check for id
    
    * return CanEditMessages

--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -1,8 +1,11 @@
 package telegram
 
 import (
+	"fmt"
+	"github.com/eko/gocache/store"
 	log "github.com/sirupsen/logrus"
 	tb "gopkg.in/lightningtipbot/telebot.v2"
+	"time"
 )
 
 func (bot TipBot) tryForwardMessage(to tb.Recipient, what tb.Editable, options ...interface{}) (msg *tb.Message) {
@@ -29,16 +32,76 @@ func (bot TipBot) tryReplyMessage(to *tb.Message, what interface{}, options ...i
 }
 
 func (bot TipBot) tryEditMessage(to tb.Editable, what interface{}, options ...interface{}) (msg *tb.Message) {
-	msg, err := bot.Telegram.Edit(to, what, options...)
+	if !allowedToPerformAction(bot, to, isAdminAndCanEdit) {
+		return
+	}
+	var err error
+	msg, err = bot.Telegram.Edit(to, what, options...)
 	if err != nil {
 		log.Warnln(err.Error())
 	}
 	return
+
 }
 
 func (bot TipBot) tryDeleteMessage(msg tb.Editable) {
+	if !allowedToPerformAction(bot, msg, isAdminAndCanDelete) {
+		return
+	}
 	err := bot.Telegram.Delete(msg)
 	if err != nil {
 		log.Warnln(err.Error())
 	}
+	return
+
+}
+
+// allowedToPerformAction will check if bot is allowed to perform an action on the tb.Editable.
+// this function will persist the admins list in cache for 5 minutes.
+// if no admins list is found for this group, bot will always fetch a fresh list.
+func allowedToPerformAction(bot TipBot, editable tb.Editable, action func(members []tb.ChatMember, me *tb.User) bool) bool {
+	switch editable.(type) {
+	case *tb.Message:
+		message := editable.(*tb.Message)
+		if message.Sender.ID == bot.Telegram.Me.ID {
+			break
+		}
+		chat := message.Chat
+		if chat.Type == tb.ChatSuperGroup || chat.Type == tb.ChatGroup {
+			admins, err := bot.Cache.Get(fmt.Sprintf("admins-%d", chat.ID))
+			if err != nil {
+				admins, err = bot.Telegram.AdminsOf(message.Chat)
+				if err != nil {
+					log.Warnln(err.Error())
+					return false
+				}
+				bot.Cache.Set(fmt.Sprintf("admins-%d", chat.ID), admins, &store.Options{Expiration: 5 * time.Minute})
+			}
+			if action(admins.([]tb.ChatMember), bot.Telegram.Me) {
+				return true
+			}
+			return false
+		}
+	}
+	return true
+}
+
+// isAdminAndCanDelete will check if me is in members list and allowed to delete messages
+func isAdminAndCanDelete(members []tb.ChatMember, me *tb.User) bool {
+	for _, admin := range members {
+		if admin.User.ID == me.ID {
+			return admin.CanDeleteMessages
+		}
+	}
+	return false
+}
+
+// isAdminAndCanEdit will check if me is in members list and allowed to edit messages
+func isAdminAndCanEdit(members []tb.ChatMember, me *tb.User) bool {
+	for _, admin := range members {
+		if admin.User.ID == me.ID {
+			return admin.CanEditMessages
+		}
+	}
+	return false
 }
