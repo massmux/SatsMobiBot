72381d007142139586bb335043957d2d7b93e8db
gohumble <55599638+gohumble@users.noreply.github.com>
2021-12-23 16:52:24 +0100

    Bunt cache (#189)
    * remove user mutex log message
    
    * add bunt cache

--- internal/storage/base.go
+++ internal/storage/base.go
@@ -1,11 +1,14 @@
 package storage
 
 import (
-	"fmt"
+	"github.com/eko/gocache/store"
+	gocache "github.com/patrickmn/go-cache"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 )
+
+var transactionCache = store.NewGoCache(gocache.New(5*time.Minute, 10*time.Minute), nil)
 
 type Base struct {
 	ID        string    `json:"id"`
@@ -50,20 +53,35 @@ func (tx *Base) Inactivate(s Storable, db *DB) error {
 }
 
 func (tx *Base) Get(s Storable, db *DB) (Storable, error) {
-	err := db.Get(s)
+	cacheTx, err := transactionCache.Get(s.Key())
 	if err != nil {
-		return s, err
+		log.Errorf("[Bunt Cache] could not get bunt object: %v", err)
+		err := db.Get(s)
+		if err != nil {
+			return s, err
+		}
+		log.Tracef("[Bunt] get object %s", s.Key())
+		return s, transactionCache.Set(s.Key(), s, &store.Options{Expiration: 5 * time.Minute})
 	}
-	if err != nil {
-		return nil, fmt.Errorf("could not get transaction")
-	}
+	log.Tracef("[Bunt Cache] get object %s", s.Key())
+	return cacheTx.(Storable), err
 
-	return s, nil
 }
 
 func (tx *Base) Set(s Storable, db *DB) error {
 	tx.UpdatedAt = time.Now()
-	return db.Set(s)
+	err := db.Set(s)
+	if err != nil {
+		log.Errorf("[Bunt] could not set object: %v", err)
+		return err
+	}
+	log.Tracef("[Bunt] set object %s", s.Key())
+	err = transactionCache.Set(s.Key(), s, &store.Options{Expiration: 5 * time.Minute})
+	if err != nil {
+		log.Errorf("[Bunt Cache] could not set object: %v", err)
+	}
+	log.Tracef("[Bunt Cache] set object: %s", s.Key())
+	return err
 }
 
 func (tx *Base) Delete(s Storable, db *DB) error {
