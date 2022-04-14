6ebb5688fd000a0395b1566acae49d40db478ef0
LightningTipBot <88730856+LightningTipBot@users.noreply.github.com>
2022-04-14 19:49:49 +0200

    new logo (#333)
    Co-authored-by: callebtc <93376500+callebtc@users.noreply.github.com>

--- internal/telegram/inline_query.go
+++ internal/telegram/inline_query.go
@@ -18,7 +18,7 @@ import (
 	tb "gopkg.in/lightningtipbot/telebot.v3"
 )
 
-const queryImage = "https://avatars.githubusercontent.com/u/88730856?v=5"
+const queryImage = "https://avatars.githubusercontent.com/u/88730856?v=6"
 
 func (bot TipBot) inlineQueryInstructions(ctx intercept.Context) (intercept.Context, error) {
 	instructions := []struct {
