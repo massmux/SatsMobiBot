a87eac37b81d99a7fd42a60afb8cb45d857db026
callebtc <93376500+callebtc@users.noreply.github.com>
2022-08-20 01:04:31 +0200

    remove

--- internal/api/payment.go
+++
@@ -1,9 +0,0 @@
-package api
-
-type PaymentStatusResponse struct {
-	State       string `json:"state"`
-	FeeMsat     int64  `json:"fee_msat,omitempty"`
-	Amount      int64  `json:"amount,omitempty"`
-	Preimage    string `json:"preimage,omitempty"`
-	PaymentHash string `json:"payment_hash"`
-}
