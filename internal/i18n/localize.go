01f4cff348c08a432776a66494b6033ebdf0e224
sifrant <43731346+sifrant@users.noreply.github.com>
2022-11-03 10:04:58 +0100

    Translation cz (#425)

--- internal/i18n/localize.go
+++ internal/i18n/localize.go
@@ -25,6 +25,7 @@ func RegisterLanguages() *i18n.Bundle {
 	bundle.LoadMessageFile("translations/fr.toml")
 	bundle.LoadMessageFile("translations/pt-br.toml")
 	bundle.LoadMessageFile("translations/tr.toml")
+	bundle.LoadMessageFile("translations/cs.toml")
 	bundle.LoadMessageFile("translations/id.toml")
 	bundle.LoadMessageFile("translations/ru.toml")
 	return bundle
