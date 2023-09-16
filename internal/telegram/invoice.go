cd41c7ec7d9b25ea2a6ae75b61392792014dba65
massmux <go@massmux.com>
2023-09-16 15:08:44 +0000

    Nepay.ch

--- internal/telegram/invoice.go
+++ internal/telegram/invoice.go
@@ -134,10 +134,10 @@ func (bot *TipBot) invoiceHandler(ctx intercept.Context) (intercept.Context, err
 	}
 
 	// check for memo in command
-	memo := "Powered by Sats.mobi @SatsMobiBot"
+	memo := "Powered by nepay.ch @NepayCHBot"
 	if len(strings.Split(m.Text, " ")) > 2 {
 		memo = GetMemoFromCommand(m.Text, 2)
-		tag := " (@SatsMobiBot)"
+		tag := " (@NepayCHBot)"
 		memoMaxLen := 159 - len(tag)
 		if len(memo) > memoMaxLen {
 			memo = memo[:memoMaxLen-len(tag)]
